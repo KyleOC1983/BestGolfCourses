b22d76ab1ada9abfc6add794ca96af5eea9a4866
unknown <kylet@LWNB02.Lockwood.local>
2020-07-21 16:27:46 -0500

    loginLogOutFirebaseAuth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BestcoursesComponent } from './components/bestcourses/bestcourses.component';
 import { NewcourseComponent } from './components/newcourse/newcourse.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo} from '@angular/fire/auth-guard';
 
+
+const redirectUnauthorizedToBestCourses = () => redirectUnauthorizedTo(['bestcourses']);
 
 const routes: Routes = [
   {path: 'bestcourses', component: BestcoursesComponent},
-  {path: 'newcourse', component: NewcourseComponent},
+  {path: 'newcourse', component: NewcourseComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToBestCourses}},
   {path: '**', redirectTo: '/bestcourses'},
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { auth } from 'firebase';
 
 
 @Component({
@@ -11,7 +15,16 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'bestgolfcourse';
   items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
+
+  constructor(firestore: AngularFirestore, public auth: AngularFireAuth, private router: Router) {
     this.items = firestore.collection('items').valueChanges();
+  }
+  //firebase Login
+  login(){
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(res=> this.router.navigate(['/bestcourses']));
+  }
+  //firebase logout
+  logout(){
+    this.auth.signOut();
   }
 }
